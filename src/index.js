0037d4d649c7f1d6b07ecb54e5d2f05441312e1e
Chloe ONeil <chloesporri@yahoo.com>
2022-12-21 20:29:15 -0800

    add basic HTML format and form

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import Age from './age.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+function handleAgeForm(event) {
+  event.preventDefault();
+  
+}
