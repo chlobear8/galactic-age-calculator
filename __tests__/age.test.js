d4d41fe89dd42bf8d2ae79ba97a72c0a10c1d1dd
Chloe ONeil <chloesporri@yahoo.com>
2022-12-21 19:14:02 -0800

    write second test for age object

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,8 +1,12 @@
 import {Age} from './../src/age.js';
 
-describe('Age', () => {
+describe('age-calculator', () => {
   test('should create an age object', () => {
     const age = new Age();
     expect(age).toBeInstanceOf(Age);
+  });
+  test('should take Earth years in constructor', () => {
+    const age = new Age(10);
+    expect(age.earthYears).toEqual(10);
   });
 });
