9b94e970aca2a773638aa5398b2e26c55995a8cd
Chloe ONeil <chloesporri@yahoo.com>
2022-12-21 20:46:38 -0800

    add UI function to index.js

--- src/index.js
+++ src/index.js
@@ -5,5 +5,20 @@ import './css/styles.css';
 
 function handleAgeForm(event) {
   event.preventDefault();
-  
+  document.querySelector('#response').innerText = null;
+  const currentAge = parseInt(document.querySelector('#current-age').value);
+  const pastAge = parseInt(document.querySelector('#years-passed').value);
+  const futureAge = parseInt(document.querySelector('#yet-passed').value);
+  const age = new Age(earthYears);
+  const mercury = age.toMercuryYears();
+  const venus = age.toVenusYears();
+  const mars = age.toMarsYears();
+  const jupiter = age.toJupiterYears();
+  const pTag = document.createElement("p");
+  pTag.append(mercury + " " + venus + " " + mars + " " + jupiter);
+  document.querySelector('#response').append(pTag);
 }
+
+window.addEventListener("load", function() {
+  this.document.querySelector("#age-calculator").addEventListener("submit", handleAgeForm);
+});
