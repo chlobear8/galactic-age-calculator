81ff1f7419f23bb56709c9bb28e6aac850b11fbf
Chloe ONeil <chloesporri@yahoo.com>
2022-12-22 14:37:30 -0800

    add response div to HTML

--- src/index.js
+++ src/index.js
@@ -9,12 +9,12 @@ function handleAgeForm(event) {
   const currentAge = parseInt(document.querySelector('#current-age').value);
   const pastAge = parseInt(document.querySelector('#years-passed').value);
   const futureAge = parseInt(document.querySelector('#yet-passed').value);
+  const planetChoice = document.querySelector('#select-planet').value;
   const age = new Age(earthYears);
-  const pTag = document.createElement("p");
-  pTag.append(age + " " + mercury + " " + venus + " " + mars + " " + jupiter);
+  
   document.querySelector('#response').append(pTag);
 }
 
 window.addEventListener("load", function() {
-  this.document.querySelector("#age-calculator").addEventListener("submit", handleAgeForm);
+  document.querySelector("#age-calculator").addEventListener("submit", handleAgeForm);
 });
